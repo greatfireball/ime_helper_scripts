111e78ec81d079f40ef5cab27850c9328bcfcf6b
Frank Förster <frank.foerster@ime.fraunhofer.de>
2017-10-17 13:25:27 +0200

    Printing single miRNA chromosomes

--- cluster_build.pl
+++ cluster_build.pl
@@ -18,3 +18,11 @@ while (<FH>)
     push(@{$chromosomes{$chr}}, {start => $start, stop => $stop, chr => $chr, strand => $strand, mirna => $mirna});
 }
 close(FH) || die "$!";
+
+foreach my $chr (keys %chromosomes)
+{
+    if (@{$chromosomes{$chr}}==1)
+    {
+	print "Skipping chromosome $chr, due to its single miRNA content.\n";
+    }
+}
