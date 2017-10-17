bdd7e5dbd9759f5001270f92b148d7204cc185a8
Frank Förster <frank.foerster@ime.fraunhofer.de>
2017-10-17 14:12:56 +0200

    Adding strand information to overall hash

--- cluster_build.pl
+++ cluster_build.pl
@@ -11,18 +11,25 @@ open(FH, "<", $inputfile) || die "$!";
 while (<FH>)
 {
     chomp;
-    s/"//g; 
+    s/"//g;
 
-    my (undef, undef, $start, $stop, $chr, $strand, $mirna)=split(",", $_); 
+    my (undef, undef, $start, $stop, $chr, $strand, $mirna)=split(",", $_);
 
-    push(@{$chromosomes{$chr}}, {start => $start, stop => $stop, chr => $chr, strand => $strand, mirna => $mirna});
+    push(@{$chromosomes{$chr}{$strand}}, {start => $start, stop => $stop, chr => $chr, strand => $strand, mirna => $mirna});
 }
 close(FH) || die "$!";
 
 foreach my $chr (keys %chromosomes)
 {
-    if (@{$chromosomes{$chr}}==1)
+    my $num_mirnas_plus = (exists $chromosomes{$chr}{'+'}) ? @{$chromosomes{$chr}{'+'}} : 0;
+    my $num_mirnas_minus = (exists $chromosomes{$chr}{'-'}) ? @{$chromosomes{$chr}{'-'}} : 0;
+    if ($num_mirnas_plus <= 1)
     {
-	print "Skipping chromosome $chr, due to its single miRNA content.\n";
+	print STDERR "Skipping chromosome $chr plus strand, due to its single miRNA content.\n";
     }
+    if ($num_mirnas_minus <= 1)
+    {
+	print STDERR "Skipping chromosome $chr minus strand, due to its single miRNA content.\n";
+    }
+
 }
