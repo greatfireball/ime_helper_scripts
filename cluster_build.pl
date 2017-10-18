4a4f445f30aa3566cd0b9d0938bfb45efea1f809
Frank Förster <frank.foerster@ime.fraunhofer.de>
2017-10-18 06:13:55 +0200

    Implement a clustering algorithm and introduce second parameter

--- cluster_build.pl
+++ cluster_build.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 
 my $inputfile = shift @ARGV;
+my $clustersize = shift @ARGV || 6000;
 
 my %chromosomes = ();
 
@@ -32,6 +33,63 @@ foreach my $chr (sort keys %chromosomes)
 	    print STDERR "Skipping chromosome $chr($strand), due to its single miRNA content.\n";
 	} elsif (@{$chromosomes{$chr}{$strand}} > 1)
 	{
+	    my @dna_strand = ();
+
+	    # mark cluster for each mirna
+	    foreach my $mirna (@{$chromosomes{$chr}{$strand}})
+	    {
+		my $start = ($mirna->{start}-$clustersize > 0) ? $mirna->{start}-$clustersize : 0;
+		my $stop  = $mirna->{stop}+$clustersize;
+		for(my $i=$start; $i<=$stop; $i++)
+		{
+		    $dna_strand[$i] = "c";
+		}
+	    }
+
+	    # find cluster borders
+	    my ($cluster_start, $cluster_stop);
+	    my @cluster = ();
+
+	    for (my $i=0; $i<@dna_strand; $i++)
+	    {
+		next unless (defined $dna_strand[$i]);
+		if ($dna_strand[$i] eq "c")
+		{
+		    $cluster_start=$i;
+		    for (my $j=$cluster_start+1; $j<@dna_strand; $j++)
+		    {
+			if (! defined $dna_strand[$j] || $dna_strand[$j] ne "c")
+			{
+			    $cluster_stop=$j-1;
+			    $i = $j;
+			    last;
+			}
+
+		    }
+
+		    unless (defined $cluster_stop)
+		    {
+			$cluster_stop = @dna_strand-1;
+			$i = $cluster_stop;
+		    }
+
+		    push(@cluster, {start => $cluster_start, stop => $cluster_stop});
+		    ($cluster_start, $cluster_stop) = (undef, undef);
+		}
+	    }
+
+	    # cluster are extracted... Find all miRNAs inside the cluster
+	    foreach my $clust (@cluster)
+	    {
+		my @clustered_miRNAs = grep {$_->{start}>=$clust->{start} && $_->{stop} <= $clust->{stop}} (@{$chromosomes{$chr}{$strand}});
+		if (@clustered_miRNAs == 1)
+		{
+		    print STDERR "Single miRNA cluster for miRNA $clustered_miRNAs[0]{mirna} on chromosome $chr($strand)\n";
+		} else {
+		    printf "Cluster of miRNAs (border-size: %d) on chromosome %s(%s) (%d-%d) containing %d miRNAs: %s\n", $clustersize, $chr, $strand, $clust->{start}, $clust->{stop}, (@clustered_miRNAs+0), join(", ", map {$_->{mirna}} (@clustered_miRNAs));
+		}
+	    }
+
 	} else {
 	    die "Should never happen";
 	}
