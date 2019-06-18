2fb83fc5e4e4927b1476b413093be18d9d1bdd57
Frank Förster <frank.foerster@ime.fraunhofer.de>
2019-06-18 12:45:18 +0200

    Importing mascot file and returning only groups belonging to the mascot files

--- mascot_demultiplex/mascot_demultiplex.pl
+++ mascot_demultiplex/mascot_demultiplex.pl
@@ -30,11 +30,26 @@ foreach my $seqfile (@seqfiles)
     }
 }
 
-my @seqs = sort { length($b) <=> length($a) || $a cmp $b } (keys (%{$mapping}));
-for(my $i=0; $i<@seqs; $i++)
+open(FH, "<", $mascot_id_file) || die "Unable to open file '$mascot_id_file': $!\n";
+my $i=0;
+while(<FH>)
 {
-    foreach my $id (sort @{$mapping->{$seqs[$i]}})
+    chomp;
+    warn "Missing sequence for $_\n" unless (exists $id2seq->{$_});
+
+    $i++;
+    foreach my $id (sort @{$mapping->{$id2seq->{$_}}})
     {
 	printf "Group_%06d\t%s\n", $i, $id;
     }
 }
+close(FH) || die "Unable to close file '$mascot_id_file: $!\n";
+
+# my @seqs = sort { length($b) <=> length($a) || $a cmp $b } (keys (%{$mapping}));
+# for(my $i=0; $i<@seqs; $i++)
+# {
+#     foreach my $id (sort @{$mapping->{$seqs[$i]}})
+#     {
+# 	printf "Group_%06d\t%s\n", $i, $id;
+#     }
+# }
