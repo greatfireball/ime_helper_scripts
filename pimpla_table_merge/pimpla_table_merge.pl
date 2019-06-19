ca0010e8520d0687729cb5260d086c66ba858e30
Frank Förster <frank.foerster@ime.fraunhofer.de>
2019-06-19 12:18:33 +0200

    Add interpro information

--- pimpla_table_merge/pimpla_table_merge.pl
+++ pimpla_table_merge/pimpla_table_merge.pl
@@ -26,9 +26,11 @@ Log::Log4perl::init( \$conf );
 my $log = Log::Log4perl::get_logger("Foo::Bar");
 
 my $mascot_id_file = "";
+my $interproscan_file = "";
 
 GetOptions(
-    "mascot=s" => \$mascot_id_file
+    "mascot=s"      => \$mascot_id_file,
+    "interpro=s"    => \$interproscan_file
     ) || die;
 
 my %mascot_ids = ();
@@ -37,7 +39,7 @@ open(FH, "<", $mascot_id_file) || die "Unable to open Mascot ID File '$mascot_id
 while(<FH>)
 {
     chomp;
-    if ($_ =~ /^.*?(\d+)\.(p\d+)/)
+    if ($_ =~ /^\S*?(\d+)\.(p\d+)/)
     {
 	my ($id, $protein) = ($1, $2);
 	push(@{$mascot_ids{$id}{orig}}, $_);
@@ -50,3 +52,30 @@ close(FH) || die "Unable to close Mascot ID File '$mascot_id_file': $!\n";
 
 $log->info(sprintf("Found %d ids from mascot run", (keys %mascot_ids)+0));
     
+open(FH, "<", $interproscan_file) || die "Unable to open Interproscan file '$interproscan_file': $!\n";
+while(<FH>)
+{
+    if ($_ =~ /^\S*?(\d+)\.(p\d+)/)
+    {
+	my ($id, $protein) = ($1, $2);
+	if (exists $mascot_ids{$id})
+	{
+	    push(@{$mascot_ids{$id}{interpro}}, $_)
+	}
+    }
+}
+close(FH) || die "Unable to close Interproscan file '$interproscan_file': $!\n";
+
+my @ids_with_interpro = ();
+foreach my $id (keys %mascot_ids)
+{
+    if (exists $mascot_ids{$id}{interpro})
+    {
+	push(@ids_with_interpro, $id);
+    } else {
+	$mascot_ids{$id}{interpro} = [];
+    }
+}
+my $num_interpros = @ids_with_interpro+0;
+my $percent_interpro = $num_interpros/((keys %mascot_ids)+0)*100;
+$log->info(sprintf("Found %d entries with interproscan information (%.1f %%)", $num_interpros, $percent_interpro));
